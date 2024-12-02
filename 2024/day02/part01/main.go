3648bd798908604ebc7747dd87dcea7a784cdfc0
lusavova <lu.savova1@gmail.com>
2024-12-02 12:14:15 +0200

    added day92

---
+++ 2024/day02/part01/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("2024/day02/input.txt")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	result := 0
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		tokens := strings.Split(line, " ")
+		numbers := []int{}
+		for _, token := range tokens {
+			num, _ := strconv.Atoi(token)
+			numbers = append(numbers, num)
+
+		}
+
+		if isSafeRow(numbers) {
+			result++
+		}
+	}
+
+	fmt.Println(result)
+}
+
+func isSafeRow(numbers []int) bool {
+	isIncreasing := numbers[1]-numbers[0] > 0
+	for i := 1; i < len(numbers); i++ {
+		prev := numbers[i-1]
+		current := numbers[i]
+		if prev == current {
+			return false
+		}
+		isCurrentIncreasing := current-prev > 0
+		if isCurrentIncreasing != isIncreasing {
+			return false
+		}
+		if absDiff(current, prev) > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func absDiff(x, y int) int {
+	diff := x - y
+	if diff < 0 {
+		return diff * -1
+	}
+
+	return diff
+}
