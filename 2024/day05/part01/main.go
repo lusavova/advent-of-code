c01723ed8cf11dd27372d583aaa81682a94e2cf3
lusavova <lu.savova1@gmail.com>
2024-12-05 11:51:23 +0200

    added day 05

---
+++ 2024/day05/part01/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, _ := os.ReadFile("./input.txt")
+	lines := strings.Split(string(data), "\n")
+
+	pagesOrder := map[string][]string{}
+	updates := [][]string{}
+	updates = parseInput(lines, updates, pagesOrder)
+
+	result := 0
+	for _, update := range updates {
+		if isValidLine(update, pagesOrder) {
+			middleElement := update[len(update)/2]
+			num, _ := strconv.Atoi(middleElement)
+			result += num
+		}
+	}
+
+	fmt.Println(result)
+}
+
+func isValidLine(line []string, pagesOrder map[string][]string) bool {
+	isValidLine := true
+	for i, el := range line {
+		for j := i + 1; j < len(line); j++ {
+			if !contains(line[j], pagesOrder[el]) {
+				isValidLine = false
+			}
+		}
+	}
+	return isValidLine
+}
+
+func contains(target string, elements []string) bool {
+	for _, el := range elements {
+		if target == el {
+			return true
+		}
+	}
+
+	return false
+}
+
+func parseInput(lines []string, updates [][]string, pagesOrder map[string][]string) [][]string {
+	indexOfNewLine := -1
+	for i, line := range lines {
+		if line == "" {
+			indexOfNewLine = i
+			continue
+		}
+		if indexOfNewLine != -1 {
+			row := strings.Split(line, ",")
+			updates = append(updates, row)
+		} else {
+			tokens := strings.Split(line, "|")
+			if _, ok := pagesOrder[tokens[0]]; !ok {
+				pagesOrder[tokens[0]] = []string{}
+			}
+			pagesOrder[tokens[0]] = append(pagesOrder[tokens[0]], tokens[1])
+		}
+	}
+	return updates
+}
---
+++ 2024/day05/part02/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, _ := os.ReadFile("./input.txt")
+	lines := strings.Split(string(data), "\n")
+
+	pagesOrder := map[string][]string{}
+	updates := [][]string{}
+	updates = parseInput(lines, updates, pagesOrder)
+
+	result := 0
+	invalidLines := [][]string{}
+	for _, update := range updates {
+		if !isValidLine(update, pagesOrder) {
+			invalidLines = append(invalidLines, update)
+		}
+	}
+
+	for _, invalidUpdate := range invalidLines {
+		orderedUpdate := getValidLine(invalidUpdate, pagesOrder)
+
+		middleElement := orderedUpdate[len(orderedUpdate)/2]
+		num, _ := strconv.Atoi(middleElement)
+		result += num
+	}
+
+	fmt.Println(result)
+}
+
+func getValidLine(line []string, pagesOrder map[string][]string) []string {
+	graph := map[string][]string{}
+
+	countDependencies := map[string]int{}
+	for _, element := range line {
+		countDependencies[element] = 0
+	}
+
+	for key, value := range pagesOrder {
+		if contains(key, line) {
+			for _, element := range value {
+				if contains(element, line) {
+					graph[key] = append(graph[key], element)
+					countDependencies[element]++
+				}
+			}
+		}
+	}
+
+	orderedLine := []string{}
+	queue := []string{}
+	for node, numOfDependencies := range countDependencies {
+		if numOfDependencies == 0 {
+			queue = append(queue, node)
+		}
+	}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		orderedLine = append(orderedLine, current)
+
+		for _, neighbor := range graph[current] {
+			countDependencies[neighbor]--
+			if countDependencies[neighbor] == 0 {
+				queue = append(queue, neighbor)
+			}
+		}
+	}
+
+	return orderedLine
+}
+
+func isValidLine(line []string, pagesOrder map[string][]string) bool {
+	isValidLine := true
+	for i, el := range line {
+		for j := i + 1; j < len(line); j++ {
+			if !contains(line[j], pagesOrder[el]) {
+				isValidLine = false
+			}
+		}
+	}
+	return isValidLine
+}
+
+func contains(target string, elements []string) bool {
+	for _, el := range elements {
+		if target == el {
+			return true
+		}
+	}
+
+	return false
+}
+
+func parseInput(lines []string, updates [][]string, pagesOrder map[string][]string) [][]string {
+	indexOfNewLine := -1
+	for i, line := range lines {
+		if line == "" {
+			indexOfNewLine = i
+			continue
+		}
+		if indexOfNewLine != -1 {
+			row := strings.Split(line, ",")
+			updates = append(updates, row)
+		} else {
+			tokens := strings.Split(line, "|")
+			if _, ok := pagesOrder[tokens[0]]; !ok {
+				pagesOrder[tokens[0]] = []string{}
+			}
+			pagesOrder[tokens[0]] = append(pagesOrder[tokens[0]], tokens[1])
+		}
+	}
+	return updates
+}
