63789a8591db9ee1cac47ef37d254920709995eb
lusavova <lu.savova1@gmail.com>
2024-12-08 19:50:50 +0200

    refactor day 03

--- 2024/day03/part01/main.go
+++ 2024/day03/part01/main.go
@@ -11,24 +11,17 @@ import (
 func main() {
 	data, _ := os.ReadFile("./input.txt")
 
+	pattern := `mul\(([0-9]+),([0-9]+)\)`
+	regex, _ := regexp.Compile(pattern)
+
 	result := 0
 	lines := strings.Split(string(data), "\n")
-	pattern := `mul\(([0-9]+),([0-9]+)\)`
-	re, err := regexp.Compile(pattern)
-	if err != nil {
-		fmt.Println("Error compiling regex:", err)
-		return
-	}
 	for _, line := range lines {
-		matches := re.FindAllStringSubmatch(line, -1)
-
+		matches := regex.FindAllStringSubmatch(line, -1)
 		for _, match := range matches {
-			if len(match) >= 3 {
-				firstNum, _ := strconv.Atoi(match[1])
-				secondNum, _ := strconv.Atoi(match[2])
-
-				result += firstNum * secondNum
-			}
+			firstNum, _ := strconv.Atoi(match[1])
+			secondNum, _ := strconv.Atoi(match[2])
+			result += firstNum * secondNum
 		}
 	}
 
--- 2024/day03/part02/main.go
+++ 2024/day03/part02/main.go
@@ -10,36 +10,34 @@ import (
 
 func main() {
 	data, _ := os.ReadFile("./input2.txt")
-	line := string(data)
+	inputData := string(data)
 
-	var finalString strings.Builder
+	var filteredData strings.Builder
 	for {
-		firstIndexDont := strings.Index(line, "don't()")
+		firstIndexDont := strings.Index(inputData, "don't()")
 		if firstIndexDont == -1 {
-			finalString.WriteString(line)
+			filteredData.WriteString(inputData)
 			break
 		}
-		substring := line[0:firstIndexDont]
-		finalString.WriteString(substring)
-		line = line[firstIndexDont:]
-		firstIndexDo := strings.Index(line, "do()")
-		line = line[firstIndexDo:]
+
+		substringUpToFirstDont := inputData[0:firstIndexDont]
+		filteredData.WriteString(substringUpToFirstDont)
+		inputData = inputData[firstIndexDont:]
+
+		firstIndexDo := strings.Index(inputData, "do()")
+		inputData = inputData[firstIndexDo:]
 	}
 
-	line = finalString.String()
-
 	pattern := `mul\(([0-9]+),([0-9]+)\)`
-	re, _ := regexp.Compile(pattern)
+	regex, _ := regexp.Compile(pattern)
+	matches := regex.FindAllStringSubmatch(filteredData.String(), -1)
+
 	result := 0
-	matches := re.FindAllStringSubmatch(line, -1)
-
 	for _, match := range matches {
-		if len(match) >= 3 {
-			firstNum, _ := strconv.Atoi(match[1])
-			secondNum, _ := strconv.Atoi(match[2])
+		firstNum, _ := strconv.Atoi(match[1])
+		secondNum, _ := strconv.Atoi(match[2])
 
-			result += firstNum * secondNum
-		}
+		result += firstNum * secondNum
 	}
 
 	fmt.Println(result)
