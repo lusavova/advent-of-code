d54ede95ddf90bb5a74666ed10318ce6dcb931e8
lusavova <lu.savova1@gmail.com>
2024-12-14 10:29:43 +0200

    added day 14; part 2: write to file

---
+++ 2024/day14/part01/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type position struct {
+	row int
+	col int
+}
+
+type velocity struct {
+	horizontal int
+	vertical   int
+}
+
+type robot struct {
+	position position
+	velocity velocity
+}
+
+const rows = 103
+const cols = 101
+
+func main() {
+	file, _ := os.Open("./input.txt")
+	defer file.Close()
+
+	var robots []robot
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+		inputPosition := strings.Split(parts[0][2:], ",")
+		positionX, _ := strconv.Atoi(inputPosition[0])
+		positionY, _ := strconv.Atoi(inputPosition[1])
+
+		inputVelocity := strings.Split(parts[1][2:], ",")
+		velocityX, _ := strconv.Atoi(inputVelocity[0])
+		velocityY, _ := strconv.Atoi(inputVelocity[1])
+		robot := robot{
+			position: position{
+				row: positionY,
+				col: positionX,
+			},
+			velocity: velocity{
+				horizontal: velocityX,
+				vertical:   velocityY,
+			},
+		}
+		robots = append(robots, robot)
+	}
+
+	var matrix [rows][cols]int
+
+	for i := 0; i < 100; i++ {
+		for i := range robots {
+			newRow, newCol := getNextPosition(rows, cols, robots[i].position, robots[i].velocity)
+			if matrix[robots[i].position.row][robots[i].position.col] > 0 {
+				matrix[robots[i].position.row][robots[i].position.col] -= 1
+			}
+			robots[i].position = position{
+				row: newRow,
+				col: newCol,
+			}
+			matrix[robots[i].position.row][robots[i].position.col] += 1
+		}
+	}
+
+	printMatrix(matrix)
+
+	result1 := 0
+	result2 := 0
+	result3 := 0
+	result4 := 0
+
+	firstRowBounds := rows / 2
+	firstColBounds := cols / 2
+
+	for row := 0; row < len(matrix); row++ {
+		for col := 0; col < len(matrix[0]); col++ {
+			if row < firstRowBounds && col < firstColBounds {
+				result1 += matrix[row][col]
+			} else if row < firstRowBounds && col > firstColBounds {
+				result2 += matrix[row][col]
+			} else if row > firstRowBounds && col < firstColBounds {
+				result3 += matrix[row][col]
+			} else if row > firstRowBounds && col > firstColBounds {
+				result4 += matrix[row][col]
+			}
+		}
+	}
+
+	fmt.Println(result1, result2, result3, result4, result1*result2*result3*result4)
+
+}
+
+func printMatrix(matrix [rows][cols]int) {
+	for row := 0; row < len(matrix); row++ {
+		for col := 0; col < len(matrix[0]); col++ {
+			if matrix[row][col] == 0 {
+				fmt.Print(".")
+				continue
+			}
+			fmt.Print(matrix[row][col])
+		}
+		fmt.Println()
+	}
+}
+
+func getNextPosition(rows, cols int, position position, velocity velocity) (int, int) {
+	newRow := 0
+	if position.row+velocity.vertical < 0 {
+		newRow = ((position.row + velocity.vertical) + rows) % rows
+	} else {
+		newRow = (position.row + velocity.vertical) % rows
+	}
+
+	newCol := 0
+	if position.col+velocity.horizontal < 0 {
+		newCol = ((position.col + velocity.horizontal) + cols) % cols
+	} else {
+		newCol = (position.col + velocity.horizontal) % cols
+	}
+
+	return newRow, newCol
+}
